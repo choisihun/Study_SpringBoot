14101794cc2ca1e52ad7970a16680fc665b507ab
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-09-13 14:12:06 +0900

    9월 13일 수업

--- guestbook/src/main/java/org/zerock/guestbook/controller/MemberController.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/MemberController.java
@@ -1,27 +1,72 @@
 package org.zerock.guestbook.controller;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.guestbook.dto.Member;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/member")
+@Slf4j
 public class MemberController {
-private Map<String, Member> userMap;
-//@PostConstruct
-//    public void init() {
-//    userMap = new HashMap<>();
-//    userMap.put("1", new Member("1", "홍박사"));
-//    userMap.put("1", new Member("2", "홍박사2"));
-//    userMap.put("1", new Member("3", "홍박사3"));
-//    userMap.put("1", new Member("4", "홍박사4"));
-//    userMap.put("1", new Member("5", "홍박사5"));
-//
-//
-//}
+    private Map<String, Member> userMap;
+    @PostConstruct
+    public void init() {
+        log.info("..... init()");
+        userMap = new HashMap<>();
+        userMap.put("1", new Member("1", "홍길동1"));
+        userMap.put("2", new Member("2", "홍길동2"));
+        userMap.put("3", new Member("3", "홍길동3"));
+        userMap.put("4", new Member("4", "홍길동4"));
+        userMap.put("5", new Member("5", "홍길동5"));
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@RequestBody Member member) {
+        userMap.put(member.getId(), member);
+    }
+    @GetMapping("/{id}")
+    public Member get(@PathVariable("id") String id) {
+        return userMap.get(id);
+    }
+
+    @GetMapping("/all")
+    public List<Member> getAll() {
+        return userMap.values().stream()
+                .collect(Collectors.toList());
+    }
+
+    //localhost:8080/member/duplicate/id?id=2
+    @GetMapping("/duplicate/id")
+    public ResponseEntity duplicate(@RequestParam("id") String id) {
+        return userMap.containsKey(id)
+                ? ResponseEntity.status(HttpStatus.CONFLICT).build() : ResponseEntity.ok().build();
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Member> modify(
+            @RequestBody Member member
+    ) {
+        Member member1 = userMap.get(member.getId());
+//        uptMember.setName(member.getName());
+//        return ResponseEntity.ok(uptMember);
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void remove(
+            @PathVariable("id") String id
+    ) {
+        userMap.remove(id);
+    }
+
+
 }
